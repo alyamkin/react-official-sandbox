d8919e296fac2a5a1a3c133035ad5f76e697e512
Andrey Lyamkin <andreylyamkin@Andreys-MacBook-Air.local>
2023-01-17 20:27:00 -0500

    Add introducing JSX

--- get-started.js
+++ get-started.js
@@ -1,2 +1,20 @@
+function formatName(user) {
+  return `${user.firstName} ${user.lastName}`;
+}
+
+const user = {
+  firstName: "Harper",
+  lastName: "Perez",
+  avatarUrl: "//unsplash.it/100/100",
+  scriptTag: <script></script>,
+};
+
+const element = (
+  <h1>
+    <img src={user.avatarUrl} />
+    Hello, {formatName(user)}! {user.scriptTag}
+  </h1>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Hello, world!</h1>);
+root.render(element);
